430620bbbd87c48de408c3604209cc37b42edb65
Felipe Dornelas <felipegabriel123dornelas@gmail.com>
2024-10-05 16:16:13 -0300

    adicionando pasta de recursividade e seus exercícios

--- Recursividade/SeisNumerosImpares.java
+++ Recursividade/SeisNumerosImpares.java
@@ -1,11 +1,26 @@
+     import java.util.Scanner;
+
 public class SeisNumerosImpares {
+
+     static Scanner s = new Scanner(System.in);
      public static void main(String[] args) {
-          
+
+          System.out.println("Digite um número inteiro positivo:");
+          int numero = s.nextInt();
+
+          if(numero % 2 == 0){ // se o número digitado pelo usuário for par, torna-o ímpar;  
+               numero ++;
+          }
+
+          somarImpares(numero, 6);
      }
-}
 
-public static void solicitarNumeroIntPos (){
+     public static void somarImpares (int numero, int contador){
 
-     System.out.println("Digite um número interiro positivo:");
-
+          if(contador == 0){
+               return;
+          }
+               System.out.println(numero);
+               somarImpares (numero + 2, contador - 1);
+     }
 }
